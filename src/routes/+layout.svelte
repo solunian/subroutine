f2c06745b442b3c655a555e89ec603a1b7f02c00
Daniel Cui <solunian@gmail.com>
2026-02-21 17:09:08 -0800

    feat: release banner

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -16,10 +16,32 @@
 
     return () => data.subscription.unsubscribe();
   });
+
+  // pre-alpha, alpha, beta, release candidate, general availability
+  const release_stage = "pre-alpha";
 </script>
 
 <svelte:head>
   <title>subroutine</title>
 </svelte:head>
 
-{@render children()}
+<div
+  class="pleated-stripes flex h-7 w-full flex-col items-center justify-center border-b border-gray-300 py-1 text-gray-500">
+  <span>{release_stage}</span>
+</div>
+
+<div class="p-2">
+  {@render children()}
+</div>
+
+<style>
+  .pleated-stripes {
+    background: repeating-linear-gradient(
+      45deg,
+      #ffeedd 0px,
+      #ffffff 15px,
+      #ffb366 15px,
+      #ffd9b3 30px
+    );
+  }
+</style>
