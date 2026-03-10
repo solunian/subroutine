fb7efe230eb0134684490d6376957f77c73842b9
Daniel Cui <solunian@gmail.com>
2026-03-09 18:34:45 -0700

    feat: lil hover border on home button

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -28,7 +28,10 @@
 
 <div class="p-2">
   <div class="py-2 font-nova text-3xl">
-    <a href="/">subroutine</a>
+    <a
+      href="/"
+      class="rounded-3xl border-2 border-gray-500/0 px-2 transition hover:border-gray-500/75"
+      >subroutine</a>
     {page.url.pathname}
   </div>
 
