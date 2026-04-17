ad59bd5a5c50d046c4a6bbbc191a69aa8751e598
Daniel Cui <solunian@gmail.com>
2026-04-17 15:33:39 -0700

    fix: remove home button jank css

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -28,10 +28,7 @@
 
 <div class="p-2">
   <div class="py-2 font-nova text-3xl">
-    <a
-      href="/"
-      class="rounded-3xl border-2 border-gray-500/0 px-2 transition hover:border-gray-500/75"
-      >subroutine</a>
+    <a href="/">subroutine</a>
     {page.url.pathname}
   </div>
 
