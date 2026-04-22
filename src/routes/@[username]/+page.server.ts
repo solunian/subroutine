0439d4bf8395693735195171a15c17c42b6bd164
Daniel Cui <solunian@gmail.com>
2026-04-22 16:20:48 -0700

    fix: promies all for async entries loading

--- src/routes/@[username]/+page.server.ts
+++ src/routes/@[username]/+page.server.ts
@@ -23,16 +23,19 @@ export const load: PageServerLoad = async ({ params, locals: { safeGetSession, s
   const subroutines = [];
   const sub_res = await supabase.from("subroutines").select("*").eq("user_id", profile_res.data.id);
   if (sub_res.error) {
-    error(500, sub_res.error.message.toLowerCase());
+    error(500, sub_res.error.message);
   }
 
-  for (const sub of sub_res.data) {
-    const entries_res = await supabase.from("entries").select("*").eq("subroutine_id", sub.id);
-    if (entries_res.error) {
+  const entries_res = await Promise.all(
+    sub_res.data.map((sub) => supabase.from("entries").select("*").eq("subroutine_id", sub.id))
+  );
+
+  for (let i = 0; i < sub_res.data.length; i++) {
+    if (entries_res[i].error) {
       // quietly errant subroutine with data fetch failed
-      subroutines.push([sub, []]);
+      subroutines.push([sub_res.data[i], []]);
     } else {
-      subroutines.push([sub, entries_res.data]);
+      subroutines.push([sub_res.data[i], entries_res[i].data]);
     }
   }
 
