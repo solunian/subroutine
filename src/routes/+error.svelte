eb2b00aa275c14e141e7c3b975364043b6d80c7a
Daniel Cui <solunian@gmail.com>
2026-05-27 02:41:30 -0700

    fix: minor error page adjustment

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -6,7 +6,9 @@
   <div
     class="flex aspect-video max-w-lg flex-col items-center gap-2 border p-16 text-center font-mono">
     <div>{page.status} error</div>
-    <div>{'<"'}{page.error?.message}{'">'}</div>
+    {#if page.error}
+      <div>{'<"'}{page.error.message}{'">'}</div>
+    {/if}
     <div>._.</div>
   </div>
 </div>
