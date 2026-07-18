3e9b66107dd8f5e670f3626478b6b83fc7cfe25c
Daniel Cui <solunian@gmail.com>
2026-07-17 18:06:37 -0700

    feat: clean up /create, animations for loading, success

--- src/lib/components/type_identicon.svelte
+++ src/lib/components/type_identicon.svelte
@@ -2,18 +2,22 @@
   import ArrowsUpDown from "$lib/icons/arrows_up_down.svelte";
   import CircleDot from "$lib/icons/circle_dot.svelte";
   import Fire from "$lib/icons/fire.svelte";
+  import PencilSquare from "$lib/icons/pencil_square.svelte";
   import QuestionMarkCircle from "$lib/icons/question_mark_circle.svelte";
-  import type { Database } from "$lib/types/database.types";
 
-  let { type }: { type: Database["public"]["Enums"]["subroutine_type"] } = $props();
+  let { type }: { type: string } = $props();
 </script>
 
-{#if type === "dot"}
-  <CircleDot />
-{:else if type === "semaphore"}
-  <ArrowsUpDown />
-{:else if type === "torch"}
-  <Fire />
-{:else}
-  <QuestionMarkCircle />
-{/if}
+<div class="inline-flex size-6 items-center justify-center">
+  {#if type === "dot"}
+    <CircleDot />
+  {:else if type === "semaphore"}
+    <ArrowsUpDown />
+  {:else if type === "torch"}
+    <Fire />
+  {:else if type === "journal"}
+    <PencilSquare />
+  {:else}
+    <QuestionMarkCircle />
+  {/if}
+</div>
